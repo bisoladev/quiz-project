725a7a3373bb03c489fc1280b1db6e3a2e28d745
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-24 18:11:39 +0100

    Created state values

--- src/context.js
+++ src/context.js
@@ -11,11 +11,19 @@ const API_ENDPOINT = "https://opentdb.com/api.php?";
 
 const url = "";
 
-const tempUrl = "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+const tempUrl =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+  const [waiting, setWaiting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [error, setError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
 };
 // make sure use
