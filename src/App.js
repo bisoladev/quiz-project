f9fac8cb286bf5c71db064e76b8e213d2c5efd79
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-24 21:57:22 +0100

    Set up question and answer UI Structure

--- src/App.js
+++ src/App.js
@@ -14,7 +14,33 @@ function App() {
     return <Loading />;
   }
 
-  return <main>Quiz App</main>;
+  const { question, incorrect_answers, correct_answer } = questions[0];
+  const answers = [...incorrect_answers, correct_answer];
+  return (
+    <main>
+      {/* <Modal /> */}
+      <section className="quiz">
+        <p className="correct-answers">
+          correct answers: {correct}/ {index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className="btn-container">
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className="answer-btn"
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                ></button>
+              );
+            })}
+          </div>
+        </article>
+        <button className="next-question">Next question</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
