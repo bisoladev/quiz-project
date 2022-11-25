41ca501ab7ac66fed0588f8b3b449be5946c78dc
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-25 21:18:55 +0100

    Randomized quiz answers

--- src/App.js
+++ src/App.js
@@ -23,7 +23,17 @@ function App() {
   }
 
   const { question, incorrect_answers, correct_answer } = questions[index];
-  const answers = [...incorrect_answers, correct_answer];
+  // const answers = [...incorrect_answers, correct_answer];
+
+  let answers = [...incorrect_answers];
+  const tempIndex = Math.floor(Math.random() * 4);
+
+  if(tempIndex === 3) {
+    answers.push(correct_answer)
+  } else {
+    answers.push(answers[tempIndex])
+    answers[tempIndex] = correct_answer
+  }
   return (
     <main>
       <Modal />
@@ -38,7 +48,8 @@ function App() {
               return (
                 <button
                   key={index}
-                  className="answer-btn" onClick={()=>checkAnswer(correct_answer === answer)}
+                  className="answer-btn"
+                  onClick={() => checkAnswer(correct_answer === answer)}
                   dangerouslySetInnerHTML={{ __html: answer }}
                 ></button>
               );
