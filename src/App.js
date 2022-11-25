9f60240d362b3bc954bd559fb8c74f39cf7a95e6
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-25 10:26:28 +0100

    correct answer functionality

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import SetupForm from "./SetupForm";
 import Loading from "./Loading";
 import Modal from "./Modal";
 function App() {
-  const { waiting, loading, questions, index, correct, nextQuestion } = useGlobalContext();
+  const {
+    waiting,
+    loading,
+    questions,
+    index,
+    correct,
+    nextQuestion,
+    checkAnswer,
+  } = useGlobalContext();
   if (waiting) {
     return <SetupForm />;
   }
@@ -30,14 +38,16 @@ function App() {
               return (
                 <button
                   key={index}
-                  className="answer-btn"
+                  className="answer-btn" onClick={()=>checkAnswer(correct_answer === answer)}
                   dangerouslySetInnerHTML={{ __html: answer }}
                 ></button>
               );
             })}
           </div>
         </article>
-        <button className="next-question" onClick={nextQuestion}>Next question</button>
+        <button className="next-question" onClick={nextQuestion}>
+          Next question
+        </button>
       </section>
     </main>
   );
